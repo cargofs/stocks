da259bd071c8f66f436f3f2c9cb3ffd8ba794ed9
cargofs <147097815+cargofs@users.noreply.github.com>
2023-11-06 23:18:41 +0300

    Try to fix cf error when fetching by IP.

--- src/routes/api/[...route]/+server.ts
+++ src/routes/api/[...route]/+server.ts
@@ -1,5 +1,6 @@
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
+import _ from 'lodash';
 import type { RequestHandler } from './$types';
 ``
 export const GET: RequestHandler = async ({ request, url, platform, fetch }) => {
@@ -14,10 +15,15 @@ export const GET: RequestHandler = async ({ request, url, platform, fetch }) => 
 
     console.log("gateway calling", { url: url.toString(), newUrl });
 
+    const headers: { [key: string]: string } = {};
+    for (const header of request.headers.keys()) {
+        headers[header] = request.headers.get(header)!;
+    }
+
     const newRequest = new Request(newUrl, {
         method: "GET",
         body: request.body,
-        headers: request.headers
+        headers: _.pick(headers, "token")
     });
 
     return await fetch(newRequest);
