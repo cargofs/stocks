9e48ece9118b97cb2218d9ba914876e11fdd4f1a
cargofs <147097815+cargofs@users.noreply.github.com>
2023-11-07 22:29:29 +0300

    No tabs.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,37 +1,37 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
 
-		interface Platform {
-			env?: {
-				/// URL of main API server, like "http://1.2.3.4:5678"
-				API_SERVER: string;
-			};
-		}
-	}
+        interface Platform {
+            env?: {
+                /// URL of main API server, like "http://1.2.3.4:5678"
+                API_SERVER: string;
+            };
+        }
+    }
 
-	interface Target {
-		path?: string;
-		name: string;
-		inner?: Target[];
-	}
+    interface Target {
+        path?: string;
+        name: string;
+        inner?: Target[];
+    }
 
-	interface TargetMod {
-		path?: string;
-		nameParts: string[];
-		inner?: TargetMod[];
-	}
+    interface TargetMod {
+        path?: string;
+        nameParts: string[];
+        inner?: TargetMod[];
+    }
 
-	namespace Data {
-		interface Price {
-			symbol: string
-			price: number
-		}
-	}
+    namespace Data {
+        interface Price {
+            symbol: string
+            price: number
+        }
+    }
 }
 
 export { };
