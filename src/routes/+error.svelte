82e1040ea754357ddc91da0009a7b5036f6de0ca
cargofs <147097815+cargofs@users.noreply.github.com>
2023-11-18 12:04:51 +0300

    Show more detailed error in dev.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { dev } from "$app/environment";
     import { page } from "$app/stores";
 </script>
 
@@ -6,4 +7,8 @@
     <h1>Ошибка {$page.status}</h1>
 
     <p><i>{$page.error?.message ?? ""}</i></p>
+
+    {#if dev}
+        <pre>Debug: $page.error: {JSON.stringify($page.error)}</pre>
+    {/if}
 </div>
